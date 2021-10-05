03541a9e4b82b0186e55ac52d17bf77b39e6d5dc
Carlos Murillo <carlos.murillo@abrigo.com>
2021-10-04 22:10:07 -0600

    IProductService.cs = Added an Interface for common API methods. ProductService.cs = Interface Implementation, resolving actual methods. ServiceResponse.cs = Added a Reponse Class with multiple properties and Template Data. SolarCoffee.Services.csproj = Added Reference to Data Project. ProductController.cs = Added Product Controller with Product Service & Get Request. SolarCoffe.Web.csproj = Added Reference to Services Project. Startup.cs = Added Transient with Interface Product Service & actual Product Service.

---
+++ SolarCoffee.Web/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SolarCoffee.Services.Product;
+
+namespace SolarCoffee.Web.Controllers
+{
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductService _productService;
+
+        public ProductController(ILogger<ProductController> logger, IProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        [HttpGet("/api/v1/product")]
+        public ActionResult GetProduct()
+        {
+            _logger.LogInformation("Getting All Products.");
+            _productService.GetAllProducts();
+            return Ok("");
+        }
+
+    }
+}
