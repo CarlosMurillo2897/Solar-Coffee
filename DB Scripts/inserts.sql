5b263c099515caab3e47db4ef11244dc5babfb01
Carlos Murillo <carlos.murillo@abrigo.com>
2021-10-05 22:36:01 -0600

    inserts.sql = Inserting mock Data into Products Table. ICustomerService.cs = Added Customer Interface with common methods. SolarCoffee.Services.csproj = Folders Created. ProductController.cs = Using Product Mapper. ProductMapper.cs = Product Mapper, it will map a Product Model into a ProductViewModel or on the other side. ProductModel.cs = Added a ProductViewModel Representation for Product Model. Inserts.sql = Mock Data Inserts into Products Table. build.bat = Added common Variables

--- DB Scripts/inserts.sql
+++ DB Scripts/inserts.sql
@@ -1,1 +1,9 @@
--- Insert into SQL.
+-- Insert into Products Table.
+INSERT INTO "Products" ("CreatedOn", "UpdatedOn", "Name", "Description", "Price", "IsTaxable", "IsArchived") 
+VALUES
+(NOW(), NOW(), '10 LB Dark Roast', '10 LB bag of dark roast coffee beans', 100, true, false),
+(NOW(), NOW(), '30 LB Dark Roast', '30 LB bag of dark roast coffee beans', 280, true, false),
+(NOW(), NOW(), '50 LB Dark Roast', '50 LB bag of dark roast coffee beans', 450, true, false),
+(NOW(), NOW(), '10 LB Light Roast', '10 LB bag of light roast coffee beans', 80, true, false),
+(NOW(), NOW(), '30 LB Light Roast', '30 LB bag of light roast coffee beans', 260, true, false),
+(NOW(), NOW(), '50 LB Light Roast', '50 LB bag of light roast coffee beans', 430, true, false);
---
+++ SolarCoffee.Web/Serialization/ProductMapper.cs
@@ -0,0 +1,47 @@
+﻿using SolarCoffee.Web.ViewModels;
+
+namespace SolarCoffee.Web.Serialization
+{
+    public class ProductMapper
+    {
+        /// <summary>
+        /// Maps a Product Data Model to ProductModel View Model.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public static ProductModel SerializeProductModel(Data.Models.Product product)
+        {
+            return new ProductModel
+            {
+                Id = product.Id,
+                CreatedOn = product.CreatedOn,
+                UpdatedOn = product.UpdatedOn,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                IsTaxable = product.IsTaxable,
+                IsArchived = product.IsArchived
+            };
+        }
+        
+        /// <summary>
+        /// Maps a ProductModel to a Product Data.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public static Data.Models.Product SerializeProductModel(ProductModel product)
+        {
+            return new Data.Models.Product
+            {
+                Id = product.Id,
+                CreatedOn = product.CreatedOn,
+                UpdatedOn = product.UpdatedOn,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                IsTaxable = product.IsTaxable,
+                IsArchived = product.IsArchived
+            };
+        }
+    }
+}
