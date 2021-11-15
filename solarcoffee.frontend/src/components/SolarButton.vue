c24f363720e5d826b3c6da2514ec179d37d5698b
Carlos Murillo <carlos.murillo@abrigo.com>
2021-11-15 17:53:46 -0600

    Using actual application name on HTML title & non script message. Added SideMenu, removed old router views. Replaced logo. Removed HelloWorld.vue component, Home & About Views. Added SideMenu Component. Added Solar Button in order to visit some hyperlink. Implemented Home & Inventory Routes. Created Global SCSS file, it will contain common CSS & pre-defined colors. Added Inventory View.

---
+++ solarcoffee.frontend/src/components/SolarButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="btn-link">
+        <button @click="visitRoute">
+            <slot></slot>
+        </button>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component, Prop } from "vue-property-decorator";
+
+@Component({
+    name: 'SolarButton',
+    components: { }
+})
+
+export default class SolarButton extends Vue {
+    @Prop({ required: false, type: String })
+    link?: string;
+
+    @Prop({ required: false, type: String })
+    id?: string;
+
+    visitRoute() {
+        this.$router.push(this.link);
+    }
+};
+</script>
