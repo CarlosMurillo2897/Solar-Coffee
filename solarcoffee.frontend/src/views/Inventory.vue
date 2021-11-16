38b0fb30c386abadf7b9ee293763938cd8917dd2
Carlos Murillo <carlos.murillo@abrigo.com>
2021-11-15 20:52:29 -0600

    Added filter for displaying numbers with dolar sign. Added Shipment Modal & Solar Modal. Added Table styles to global.scss file. Implemented Product & Product Inventory Interfaces. Implemented MockData for Inventory, Table & Show Modal Buttons.

--- solarcoffee.frontend/src/views/Inventory.vue
+++ solarcoffee.frontend/src/views/Inventory.vue
@@ -2,6 +2,15 @@
   <div class="inventory-container">
     <h1 id="inventoryTitle">Inventory Dashboard</h1>
     <hr />
+
+    <div class="inventory-actions">
+      <solar-button @click.native="showNewProductModal" id="addNewBtn">
+        Add New Item
+      </solar-button>
+      <solar-button @click.native="showShipmentModal" id="receiveShipmentBtn">
+        Receive Shipment
+      </solar-button>
+    </div>
     <table id="inventoryTable" class="table">
       <tr>
         <th>Item</th>
@@ -10,19 +19,76 @@
         <th>Taxable</th>
         <th>Delete</th>
       </tr>
-      <!-- <tr v-for="item in inventory"></tr> -->
+      <tr v-for="item in inventory" :key="item.id">
+        <td>{{ item.product.name }}</td>
+        <td>{{ item.quantityOnHand }}</td>
+        <td>{{ item.product.price | price }}</td>
+        <td>
+          <span v-if="item.product.isTaxable"> Yes </span>
+          <span v-else> No </span>
+        </td>
+        <td>
+          <div>X</div>
+        </td>
+      </tr>
     </table>
+    <new-product-modal v-if="isNewProductVisible" />
+    <shipment-modal v-if="isShipmentVisible" :inventory="inventory" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
+// Types.
+import { IProductInventory } from "@/types/Product";
+
+// Components.
+import SolarButton from "@/components/SolarButton.vue";
+import ShipmentModal from "@/components/modals/ShipmentModal.vue";
+
 @Component({
   name: "Inventory",
-  components: {},
+  components: { SolarButton, ShipmentModal },
 })
 export default class Inventory extends Vue {
-  // inventory: IProductInventory[] = [];
+  isNewProductVisible = false;
+  isShipmentVisible = false;
+  inventory: IProductInventory[] = [
+    {
+      id: 1,
+      product: {
+        id: 1,
+        name: "Some Product",
+        description: "Good stuff",
+        price: 100,
+        createdOn: new Date(),
+        updatedOn: new Date(),
+        isTaxable: true,
+        isArchived: false,
+      },
+      quantityOnHand: 100,
+      idealQuantity: 100,
+    },
+    {
+      id: 2,
+      product: {
+        id: 2,
+        name: "Another Product",
+        description: "Good stuff",
+        price: 100,
+        createdOn: new Date(),
+        updatedOn: new Date(),
+        isTaxable: false,
+        isArchived: false,
+      },
+      quantityOnHand: 40,
+      idealQuantity: 20,
+    },
+  ];
+
+  showNewProductModal(): void {}
+
+  showShipmentModal(): void {}
 }
 </script>
