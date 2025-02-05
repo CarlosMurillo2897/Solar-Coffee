d23acf368789ceee2723873536cce4af0c0a96cf
Murillo Carlos <cmb2897@gmail.com>
2025-02-05 17:31:13 -0600

    Adding Migrations. Removing batch files, using Makefile instead. Renaming Scripts and moving it into Scripts folder with Makefile. Adding owned and schemas to user creation.

--- Scripts/DB/Create.sql
+++ Scripts/DB/Create.sql
@@ -1,7 +1,8 @@
 -- REMOVE Warning Messages. Example: "NOTICE: database|role 'name' does not exist, skipping".
 SELECT set_config('client_min_messages', 'warning', false);
 
---DROP DB & USER IF ALREADY EXISTS.
+--DROP OWNED, DB & USER IF ALREADY EXISTS.
+DROP OWNED BY solardev;
 DROP DATABASE IF EXISTS solardev;
 DROP USER IF EXISTS solardev;
 
@@ -9,3 +10,7 @@ DROP USER IF EXISTS solardev;
 CREATE USER solardev with PASSWORD 'solar123';
 CREATE DATABASE solardev;
 GRANT ALL PRIVILEGES ON DATABASE solardev TO solardev;
+
+\c solardev postgres
+-- You are now connected to database "solardev" as user "postgres".
+GRANT ALL ON SCHEMA public TO solardev;
